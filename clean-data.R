64c8c45b24dbf042430210d395d7fa72fc8469f4
Susan Vanderplas <srvanderplas@gmail.com>
2023-02-21 08:32:52 -0600

    Homework 5 update

--- clean-data.R
+++ clean-data.R
@@ -1,3 +1,5 @@
+# This code reads in the data from the groundhog-day.com api and saves it to two CSV files.
+
 library(jsonlite)
 library(tidyverse)
 
@@ -10,3 +12,8 @@ groundhogs <- tmp$groundhogs %>%
 predictions <- select(groundhogs, id, name, lat, long, isGroundhog, active, predictions) %>%
   unnest(predictions)
 
+write_csv(predictions, "groundhog-predictions.csv")
+
+groundhogs %>%
+  select(-predictions) %>%
+  write_csv("groundhogs.csv")
